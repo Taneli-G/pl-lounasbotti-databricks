337c4553cfec8ffbb21e0ed8666a6e16f5d59a17
Taneli-G <Taneli-G>
2024-01-29 10:14:28 +0000

    Created extract for restaurant menus

--- pl-lounas-bot/Extract restaurant menus.py
+++ pl-lounas-bot/Extract restaurant menus.py
@@ -1,13 +1,13 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC # Hae lounasbotin käyttäjien arviot MiniPossusta
+# MAGIC # Hae lounasbotin lounaslistat MiniPossusta
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC ### JDBC connection string MiniPossuun
 # MAGIC
-# MAGIC MiniPossun ylläpitäjä: Juuso Paakkunainen (juuso.paakkunainen@productivityleap.com) (5.12.2023)
+# MAGIC MiniPossun ylläpitäjä: Juuso Paakkunainen (juuso.paakkunainen@productivityleap.com) (29.1.2024)
 
 # COMMAND ----------
 
@@ -16,7 +16,7 @@ driver = "org.postgresql.Driver"
 database_host = "ec2-18-203-205-71.eu-west-1.compute.amazonaws.com"
 database_port = "5432"
 database_name = "d9j5rds7v76as3"
-table = "public.user_reactions"
+table = "public.menus"
 user = "cezsfrpwjbrrxo"
 password = "107c5f821bb4f1701e3d6d76c8cba3eca000ab6fee6d4f68df562e418528bd09"
 
@@ -31,10 +31,10 @@ url = f"jdbc:postgresql://{database_host}:{database_port}/{database_name}"
 
 # COMMAND ----------
 
-source_table_name = "user_reviews"
-target_table_name = "pl_lounas_bot.user_reviews.user_reviews_bronze"
-spark.conf.set("user_reviews.target_table_name", target_table_name)
-spark.conf.set("user_reviews.source_table_name", source_table_name)
+source_table_name = "menus"
+target_table_name = "pl_lounas_bot.menus.menus_bronze"
+spark.conf.set("menus.target_table_name", target_table_name)
+spark.conf.set("menus.source_table_name", source_table_name)
 
 # COMMAND ----------
 
@@ -49,7 +49,7 @@ remote_table = (spark.read
 )
 
 # Luo väliaikainen näkymä lähdejärjestelmän user_reviews taulusta
-remote_table.createOrReplaceTempView('user_reviews_src')
+remote_table.createOrReplaceTempView('menus_src')
 
 # COMMAND ----------
 
@@ -58,30 +58,27 @@ remote_table.createOrReplaceTempView('user_reviews_src')
 # MAGIC Seuraava SQL
 # MAGIC 1. muodostaa väliaikaisen näkymän lähdejärjestelmän taulusta,
 # MAGIC 2. luo Bronze-taulun mikäli sitä ei ole olemassa ja
-# MAGIC 3. Suorittaa mergen lähdejärjestelmän näkymästä Bronze-tauluun, tallettaen *jokaisen* uuden ja muuttuneen lounasarvion **uutena rivinä**.
+# MAGIC 3. Suorittaa mergen lähdejärjestelmän näkymästä Bronze-tauluun, tallettaen *jokaisen* uuden ja muuttuneen ravintolan ruokalistan **uutena rivinä**.
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC CREATE OR REPLACE TEMP VIEW ${user_reviews.source_table_name} AS 
-# MAGIC SELECT * FROM user_reviews_src;
+# MAGIC CREATE OR REPLACE TEMP VIEW ${menus.source_table_name} AS 
+# MAGIC SELECT * FROM menus_src;
 # MAGIC
-# MAGIC CREATE TABLE IF NOT EXISTS ${user_reviews.target_table_name} (
+# MAGIC CREATE TABLE IF NOT EXISTS ${menus.target_table_name} (
 # MAGIC   id INT
-# MAGIC   ,user_id BIGINT
-# MAGIC   ,message_id BIGINT
-# MAGIC   ,reaction_type STRING
-# MAGIC   ,created_at TIMESTAMP
+# MAGIC   ,date DATE
+# MAGIC   ,menu_raw STRING
 # MAGIC   ,restaurant_name STRING
+# MAGIC   ,timestamp TIMESTAMP
 # MAGIC );
 # MAGIC
-# MAGIC MERGE INTO ${user_reviews.target_table_name} trg
-# MAGIC USING ${user_reviews.source_table_name} src
-# MAGIC ON src.id = trg.id AND src.created_at = trg.created_at
-# MAGIC WHEN NOT MATCHED 
+# MAGIC MERGE INTO ${menus.target_table_name} trg
+# MAGIC USING ${menus.source_table_name} src
+# MAGIC ON src.id = trg.id AND src.timestamp = trg.timestamp
+# MAGIC WHEN NOT MATCHED
 # MAGIC   THEN INSERT *;
-# MAGIC
-# MAGIC
 
 # COMMAND ----------
 
--- pl-lounas-bot/User Reviews - DLT.sql
+++ pl-lounas-bot/User Reviews - DLT.sql
@@ -6,8 +6,9 @@
 
 -- MAGIC %md
 -- MAGIC ### USER_REVIEWS_BRONZE_CLEAN
--- MAGIC Siivoa bronze-taulun lounasarviot ja luo DLT-streaming taulu.
--- MAGIC **user_reviews_bronze_clean** sisältää ainoastaan valideja käyttäjäarvioita.
+-- MAGIC - **Source:** user_reviews_bronze
+-- MAGIC
+-- MAGIC Siivoa ja validoi bronze-taulun lounasarviot.
 
 -- COMMAND ----------
 
@@ -32,8 +33,11 @@ SELECT * FROM STREAM(pl_lounas_bot.user_reviews.user_reviews_bronze);
 
 -- MAGIC %md
 -- MAGIC ### USER_REVIEWS_SILVER
--- MAGIC #### SCD Type 1
--- MAGIC Seuraava streaming-DLT taulu saa tietonsa user_reviews_bronze_clean -taulusta ja poimii sieltä ainoastaan viimeisimmällä timestampilla olevat tietueet per arvio-id. 
+-- MAGIC
+-- MAGIC - **Source**: user_reviews_bronze_clean
+-- MAGIC - **SCD:** Type 1
+-- MAGIC
+-- MAGIC Hae tiedot user_reviews_bronze_clean -taulusta ja poimii sieltä ainoastaan viimeisimmällä timestampilla olevat tietueet per arvio-id. 
 
 -- COMMAND ----------
 
@@ -44,3 +48,75 @@ APPLY CHANGES INTO LIVE.user_reviews_silver
   KEYS (id)
   APPLY AS DELETE WHEN 1=2
   SEQUENCE BY created_at
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### USER_REVIEWS_STATS_DAILY_GOLD
+-- MAGIC - **Source:** user_reviews_silver
+-- MAGIC
+-- MAGIC Ravintoloiden arvioiden statistiikkaa per päivä. Ravintola yksilöitynä.
+
+-- COMMAND ----------
+
+CREATE OR REFRESH LIVE TABLE user_reviews_stats_daily_gold AS
+SELECT
+  restaurant_name
+  ,to_date(created_at) AS date
+  ,year(created_at) AS year
+  ,month(created_at) AS month
+  ,day(created_at) AS day
+  ,count_if(reaction_type LIKE 'like') as positive_reviews
+  ,count_if(reaction_type LIKE 'neutral') as neutral_reviews
+  ,count_if(reaction_type LIKE 'dislike') as negative_reviews
+  ,count(reaction_type) AS total_reviews
+FROM LIVE.user_reviews_silver
+GROUP BY
+  restaurant_name
+  ,date
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### USER_REVIEWS_RESTAURANT_STATS_MONTHLY_GOLD
+-- MAGIC - **Source:** user_reviews_silver
+-- MAGIC
+-- MAGIC Ravintoloiden edistyneempää statistiikkaa per kuukausi. Ravintola yksilöitynä.
+
+-- COMMAND ----------
+
+CREATE OR REFRESH LIVE TABLE user_reviews_restaurant_stats_monthly_gold AS
+SELECT
+  restaurant_name
+  ,year(created_at) AS year
+  ,month(created_at) AS month
+  ,count_if(reaction_type LIKE 'like') as positive_reviews
+  ,count_if(reaction_type LIKE 'neutral') as neutral_reviews
+  ,count_if(reaction_type LIKE 'dislike') as negative_reviews
+  ,count(reaction_type) AS total_reviews
+  ,positive_reviews / total_reviews AS positives_of_total
+  ,neutral_reviews / total_reviews AS neutrals_of_total
+  ,negative_reviews / total_reviews AS negatives_of_total
+FROM LIVE.user_reviews_silver silver
+GROUP BY
+  restaurant_name
+  ,year
+  ,month
+
+-- COMMAND ----------
+
+CREATE OR REFRESH LIVE TABLE user_reviews_total_monthly_gold AS
+SELECT
+  year(created_at) AS year
+  ,month(created_at) AS month
+  ,count_if(reaction_type LIKE 'like') as positive_reviews
+  ,count_if(reaction_type LIKE 'neutral') as neutral_reviews
+  ,count_if(reaction_type LIKE 'dislike') as negative_reviews
+  ,count(reaction_type) AS total_reviews
+  ,positive_reviews / total_reviews AS positives_of_total
+  ,neutral_reviews / total_reviews AS neutrals_of_total
+  ,negative_reviews / total_reviews AS negatives_of_total
+FROM LIVE.user_reviews_silver
+GROUP BY
+  year
+  ,month;
